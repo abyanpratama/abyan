a04f5708dc103fd064700ca20cad4c5f8784a43e
abyanpratama <abyanpratama421@gmail.com>
2025-01-24 23:49:29 +0700

    Revert "kocak"

--- latihan4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while true; do
-
-	telnet google.com 80 >> kocak.log 2>&1	
-pkill telnet 
-	sleep 5s
-
-done
--- latihan6.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-hari=$(date +"%d-%m-%Y")
-echo input " $hari "
-
-tanggal=$(date +%s)
-echo output " $tanggal "
